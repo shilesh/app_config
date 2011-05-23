d93d393fedfdb2ac07456edb77904a9c629a820c
Shilesh <shilesh.kumar@gmal.com>
2011-05-23 17:57:43 +0530

    Initial draft with basic functionality

---
+++ lib/app_config.rb
@@ -0,0 +1,22 @@
+#Load app_constants YML file
+module AppConfig
+  config_file = File.join(RAILS_ROOT, "/config/app_config.yml")
+  if File.exists? config_file
+    begin
+      APP_CONSTANTS = YAML.load_file(config_file) ? YAML.load_file(config_file)[RAILS_ENV] : []  
+      APP_CONSTANTS.each do |const|
+        AppConfig.const_set(const[0].upcase, const[1])
+      end
+    rescue Exception => e
+      puts "********** AppConfig : ERROR ****************"
+      puts "** Error reading #{config_file}"
+      puts "** #{e.message}"
+      puts "*********************************************"
+    end  
+  else
+     puts "************************** AppConfig : WARNING *****************************"
+     puts "** Can not load values from config file "
+     puts "** #{config_file} is missing "
+     puts "****************************************************************************"
+  end
+end
