3815306421e95c39c3a099c8573a29e757b66316
shilesh <shilesh.kumar@gmail.com>
2011-05-24 11:18:10 +0530

    Adding tests

--- test/app_config_test.rb
+++ test/app_config_test.rb
@@ -1,8 +1,9 @@
 require 'test_helper'
 
 class AppConstantsTest < ActiveSupport::TestCase
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
+
+  def test_should_load_config_values
+    assert_equal AppConfig::NAME, "TestUser"
   end
+  
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,3 +1,8 @@
 require 'rubygems'
 require 'test/unit'
 require 'active_support'
+
+ENV['RAILS_ENV'] = 'test'
+ENV['RAILS_ROOT'] ||= File.dirname(__FILE__) + '/../../../..'
+
+require File.expand_path(File.join(ENV['RAILS_ROOT'], 'config/environment.rb'))
