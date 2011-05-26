7faa6ea5b64e7d14d893609b489215e82afee039
shilesh <shilesh.kumar@gmail.com>
2011-05-26 18:52:27 +0530

    When you uninstall the files in config/app_config/* will be retained, expecting that it would contain usefull data. You can manually delete it if needed

--- uninstall.rb
+++ uninstall.rb
@@ -1,4 +1,5 @@
 # Uninstall hook code here
-p __FILE__ 
-`rm -rf config/app_config`
-puts " 'config/app_config/*' removed"
+#`rm -rf config/app_config`
+puts "******** App Config ***************"
+puts "'config/app_config/*' file are (if any) retained. You can manually remove them if not needed."
+puts "***********************************"
