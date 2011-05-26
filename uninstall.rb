22d9e19d6fc8b6834f7605cc2ae06f449add6ebb
shilesh kumar <shilesh.kumar@gmail.com>
2011-05-26 06:28:39 -0700

    Message updated for retaining config file while Uninstalling the plugin.

--- uninstall.rb
+++ uninstall.rb
@@ -1,5 +1,5 @@
 # Uninstall hook code here
 #`rm -rf config/app_config`
 puts "******** App Config ***************"
-puts "'config/app_config/*' file are (if any) retained. You can manually remove them if not needed."
+puts "Config file @ 'config/app_config' will be (if any) retained. You can manually remove them if not needed."
 puts "***********************************"
